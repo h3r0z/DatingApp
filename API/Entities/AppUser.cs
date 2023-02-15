96c17e84e6d9e53b8ae75684f5eaa7741b0f45a3
Danjie Zhou <danjiezhou@hotmail.com>
2023-02-15 16:42:24 +0100

    First Commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+// using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //use this if u don t use Id 
+        // [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
