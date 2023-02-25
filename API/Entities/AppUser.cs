4e3645e267dae69c2a3e9400e74eaf99c34e5502
Danjie Zhou <danjiezhou@hotmail.com>
2023-02-25 20:56:48 +0100

    Break

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,7 @@
 // using System.ComponentModel.DataAnnotations;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class AppUser
@@ -7,6 +9,11 @@ namespace API.Entities
         //use this if u don t use Id 
         // [Key]
         public int Id { get; set; }
+
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
